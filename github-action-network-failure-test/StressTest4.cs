1ad13ae7c6707931cc4c19f824345d427ee2c9e0
Timothy Baldridge <tbaldridge@gmail.com>
2023-07-26 15:48:38 -0600

    Trying to get the tests to break

---
+++ github-action-network-failure-test/StressTest4.cs
@@ -0,0 +1,31 @@
+using System.Security.Cryptography;
+using FluentAssertions;
+
+namespace github_action_network_failure_test;
+
+public class StressTest4
+{
+    private readonly LocalHttpServer _server;
+    private readonly HttpClient _client;
+
+    public StressTest4(LocalHttpServer server)
+    {
+        _server = server;
+        _client = new HttpClient();
+    }
+    
+    [Fact]
+    public async Task TestManyConnections()
+    {
+        for (var x = 0; x < 1000; x++)
+        {
+            using var res = await _client.GetAsync(_server.Uri + Random.Shared.Next().ToString(), HttpCompletionOption.ResponseHeadersRead);
+            res.IsSuccessStatusCode.Should().BeTrue();
+
+            await using var stream = await res.Content.ReadAsStreamAsync();
+            var ms = new MemoryStream();
+            await stream.CopyToAsync(ms);
+            SHA256.HashData(ms.ToArray()).Should().BeEquivalentTo(_server.LargeDataHash);
+        }
+    }
+}
